a776b0a8c20246bfd4857036fbdf5a4e77c3061b
sachinvayalakara <56108759+sachinvayalakara@users.noreply.github.com>
2020-11-20 16:32:10 +0530

    Update views.py

--- Base/views.py
+++ Base/views.py
@@ -3,16 +3,23 @@ import inflect
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
+import pyttsx3
+import os 
+
 # Create your views here.
 def register(request):
     try:
+        x = 0
+        number=0
         if request.method == 'POST':
             number = request.POST['number']
             p = inflect.engine()
-            x=p.number_to_words(number)
-            print(x)
-
-        return render(request, 'register.html',{'x':x})
+            x = p.number_to_words(number)
+            engine = pyttsx3.init()
+            text = x
+            engine.say(text) 
+            engine.runAndWait()
+        return render(request, 'register.html',{'x':x,'number':number})
 
     except Exception as e:
         print(e)
